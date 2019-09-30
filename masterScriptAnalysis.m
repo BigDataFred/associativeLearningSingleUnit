ea0ebb52b390b1a6b4a7a329fa2885cf18b06d0a
Frederic Roux (Psychology) <rouxf@les-lnx-fs05.bham.ac.uk>
2019-09-30 19:35:41 +0100

    fixed bug in stratMode query

--- masterScriptAnalysis.m
+++ masterScriptAnalysis.m
@@ -24,8 +24,8 @@ alpha = 0.05;
 %spk2LFPcouplingScriptEMtask( pId, expMode, spkMode, spk2LFPmode, nRand, alpha, savePath, rdsPath );
 
 %% compute spk 2 lfp coupling for hits vs misses
-stratMode = {'On', 'On'};
+stratMode = {'On'};
 spk2lfCoupling4HitsANDmisses( pId, expMode, spkMode, spk2LFPmode, rdsPath, savePath, alpha, nRand , stratMode);
 
-stratMode = {'Off','Off'};
+stratMode = {'Off'};
 spk2lfCoupling4HitsANDmisses( pId, expMode, spkMode, spk2LFPmode, rdsPath, savePath, alpha, nRand , stratMode);
